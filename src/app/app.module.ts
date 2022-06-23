a7abf06f579b13201ed55519bcec6f296a36af0e
zjy960812 <1120957260@qq.com>
2022-06-23 08:06:54 +0800

    测试sass

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,23 +10,25 @@ import zh from '@angular/common/locales/zh';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NzButtonModule } from 'ng-zorro-antd/button';
-import { NzLayoutModule } from 'ng-zorro-antd/layout';
-import { NzMenuModule } from 'ng-zorro-antd/menu';
+// import { NzButtonModule } from 'ng-zorro-antd/button';
+// import { NzLayoutModule } from 'ng-zorro-antd/layout';
+// import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { Test1Component } from './test1/test1.component';
+import { Test2Component } from './test2/test2.component';
 
 registerLocaleData(zh);
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, Test1Component, Test2Component],
   imports: [
-    NzMenuModule,
+    // NzMenuModule,
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
-    NzButtonModule,
-    NzLayoutModule,
+    // NzButtonModule,
+    // NzLayoutModule,
   ],
   providers: [{ provide: NZ_I18N, useValue: zh_CN }],
   bootstrap: [AppComponent],
